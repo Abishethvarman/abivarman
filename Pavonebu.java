256e2f540f25ef5c31796f4f7c4b4230e8022386
Abishethvarman V <57715922+Abishethvarman@users.noreply.github.com>
2022-01-18 00:55:18 +0530

    Add files via upload

---
+++ Pavonebu.java
@@ -0,0 +1,18 @@
+import.java.util.Scanner;
+public class Pavonebu{
+	public static void main{
+		int num2;
+		Scanner.in= new Scanner(System.in);
+		System.out.println("Enter any number");
+		int num= in.nextInt();
+		
+		if num=>0 {
+		System.out.println(+num);
+		}
+		else{
+			num2=(-1)*num;
+			System.out.println(+num2);
+			
+			{
+	}
+}
